96c26719a10e12efc24f78d9f504a83a1719c0e1
thezj <thezj@qq.com>
2018-10-31 15:05:55 +0800

    管理节点层级

--- assets/Script/HelloWorld.js
+++ assets/Script/HelloWorld.js
@@ -12,17 +12,45 @@ cc.Class({
         armerorigin: {
             type: cc.SpriteFrame,
             default: null
+        },
+        stageback: {
+            type: cc.Node,
+            default: null
         }
     },
 
     // use this for initialization
     onLoad: function () {
+
+
         this.armerorigin = this.node.getComponents(cc.Sprite)[0].spriteFrame
+
+        console.log('stage节点和当前节点zindex', this.stageback.zIndex, this.node.zIndex)
+        this.stageback.zIndex = 0
+        this.node.zIndex = 1
+        let oddnumber = 0
+
         setInterval(i => {
+
+            //切换sprite的贴图
             if (this.node.getComponents(cc.Sprite)[0].spriteFrame == this.armerorigin) {
                 this.node.getComponents(cc.Sprite)[0].spriteFrame = this.armerright
             } else {
                 this.node.getComponents(cc.Sprite)[0].spriteFrame = this.armerorigin
+            }
+
+            //切换节点的层级
+            if (oddnumber == 0) {
+                oddnumber = 1
+                if (this.node.zIndex == 1) {
+                    this.stageback.zIndex = 1
+                    this.node.zIndex = 0
+                } else {
+                    this.stageback.zIndex = 0
+                    this.node.zIndex = 1
+                }
+            } else {
+                oddnumber = 0
             }
 
         }, 300)
