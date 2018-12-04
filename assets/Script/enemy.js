ff8eebd37e4d67a083be668f6a8847a5f0e96576
thezj <thezj@qq.com>
2018-12-04 18:31:58 +0800

    使用noodpool

--- assets/Script/enemy.js
+++ assets/Script/enemy.js
@@ -12,7 +12,7 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        enemyPrefab: cc.Prefab
+
         // foo: {
         //     // ATTRIBUTES:
         //     default: null,        // The default value will be used only when the component attaching
@@ -34,34 +34,15 @@ cc.Class({
 
     // onLoad () {},
 
-    onLoad() {
+    start() {
 
-        this.enemypool = new cc.NodePool()
-        let initcount = 5
-        for (let i = 0; i < initcount; i++) {
-            let enemyinstance = cc.instantiate(this.enemyPrefab)
-            this.enemypool.put(enemyinstance)
-        }
-
+     
     },
 
-    createEnemy: function (parentNode) {
-        let enemy = null;
-        if (this.enemypool.size() > 0) { // 通过 size 接口判断对象池中是否有空闲的对象
-            enemy = this.enemypool.get();//从对象池获取一个敌人对象
-        } else { // 如果没有空闲对象，也就是对象池中备用对象不够时，我们就用 cc.instantiate 重新创建
-            enemy = cc.instantiate(this.enemyPrefab);
-        }
-        enemy.parent = this.node; // 将生成的敌人加入节点树
-        enemy.getComponent('enemy').init(); 
-    },
-
-    onEnemyKilled: function (enemy) {
-
-        console.log(this.enemypool.size(),'========================')
-        // enemy 应该是一个 cc.Node
-        this.enemypool.put(enemy);
-        // 和初始化时的方法一样，将节点放进对象池，这个方法会同时调用节点的 removeFromParent
+    init(){
+           setTimeout(i => {
+            this.node.parent.getComponent('player').onEnemyKilled(this.node)
+        }, 1000)
     }
 
     // update (dt) {},
